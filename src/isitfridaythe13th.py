b270aefb0c03da6054c817d9c5dd5c475d0765b6
aj <amanjeev@gmail.com>
2011-06-13 23:50:15 -0400

    Done

--- src/isitfridaythe13th.py
+++ src/isitfridaythe13th.py
@@ -4,7 +4,11 @@ now = datetime.datetime.now()
 
 print 'Content-Type: text/html'
 print ''
-print '<!DOCTYPE html><html lang ="en"><head></head><body style="text-align: center; padding-top: 200px;"><div class="content" style="font-weight: bold; font-size: 220px; font-family: Arial,sans-serif; text-decoration: none; color: black;">'
+print '<!DOCTYPE html>\
+        <html lang ="en">\
+        <head></head>\
+        <body style="text-align: center; padding-top: 200px;">\
+            <div class="content" style="font-weight: bold; font-size: 220px; font-family: Arial,sans-serif; text-decoration: none; color: black;">'
 
 if now.weekday() == 4 and now.day == 13:
     print 'YES'
