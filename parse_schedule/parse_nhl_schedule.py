c49bae1d13c568faca51b0bc898495d25b6d44f7
martbhell <martbhell+github@gmail.com>
2015-06-09 21:45:06 +0300

    parser: Now with a teamdates dictionary!

--- parse_schedule/parse_nhl_schedule.py
+++ parse_schedule/parse_nhl_schedule.py
@@ -12,15 +12,13 @@ soup = BeautifulSoup(page.read())
 data = []
 data1 = []
 lines = []
-# Thought experiment:
-# Format1 test: { "CHI" : [ "Mon Jun 8, 2015", "Wed Jun 10, 2015" ], "TBL" : [ "Mon Jun 8, 2015", "Wed Jun 10, 2015" ] }
-# Format2 test: { "Wed Jun 8, 2015" : [ "CHI", "TBL" ] }
+# Format2 test: { "Wed Jun 8, 2015" : [ "Tampa Bay", "Chicago" ] }
 teamdates = {}
 table = soup.find('table', attrs={'class':'data schedTbl'})
 table_body = table.find('tbody')
 
-#print table_body
 
+# Just get dates and put them in a list:
 rows = table_body.findAll('tr')
 for row in rows:
     # http://stackoverflow.com/questions/23377533/python-beautifulsoup-parsing-table
@@ -30,17 +28,26 @@ for row in rows:
     data.append([ele for ele in cols if ele]) # Get rid of empty values
 
     # This finds one specific div in the row
-    divs = row.find("div", {"class": "skedStartDateSite"})
-    divs = [ele1.strip() for ele1 in divs]
-    data1.append([ele for ele in divs if ele]) # Get rid of empty values
+    date = row.find("div", {"class": "skedStartDateSite"})
+    date = [ele1.strip() for ele1 in date]
+    data1.append([ele for ele in date if ele]) # Get rid of empty values
+
+    # date and teams playing that date:  first a <div class=teamName"> <a ..>Detroit</a></div>
+    tivs = row.findAll("div", {"class": "teamName"})
+    data2 = []
+    for a in tivs:
+        data2.append(a.find('a').contents[0])
+    teamdates[date[0]] = data2
+
+print teamdates
 
 # We want this in a list, like:
 # lines = ['Sat Jun 6, 2015', 'Mon Jun 8, 2015', 'Wed Jun 10, 2015', 'Sat Jun 13, 2015', 'Mon Jun 15, 2015', 'Wed Jun 17, 2015', '']
 
 for tr in data1:
-    date = tr[0]
-    date.encode("UTF8")
-    lines.append(date)
+    d8 = tr[0]
+    d8.encode("UTF8")
+    lines.append(d8)
 
 # Using the list as it is:
 print lines[0]
