e1bb04a1aa7fe83ddd0d6564d5821e02749a2a34
martbhell <martbhell+github@gmail.com>
2015-06-11 21:11:38 +0300

    Now with forkme :)

--- src/wasthereannhlgamelastnight.py
+++ src/wasthereannhlgamelastnight.py
@@ -50,7 +50,9 @@ class MainPage(webapp2.RequestHandler):
                 self.response.write("NO")
 
         self.response.write('<div class="disclaimer" style="font-size:10px; ">')
-        self.response.write(now2)
+        # maybe https://github.com/simonwhitaker/github-fork-ribbon-css is better..
+        self.response.write('<a href="https://github.com/martbhell/wasthereannhlgamelastnight"><img style="position: absolute; top: 0; left: 0; border: 0;" src="https://camo.githubusercontent.com/567c3a48d796e2fc06ea80409cc9dd82bf714434/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_left_darkblue_121621.png"></a>')
+        #self.response.write(now2)
         self.response.write('</div>\n')
         self.response.write('<!-- NHL.com is the official web site of the National Hockey League. NHL, the NHL Shield, the word mark and image of the Stanley Cup, Center Ice name and logo, NHL Conference logos are registered trademarks. All NHL logos and marks and NHL team logos and marks depicted herein are the property of the NHL and the respective teams. This website is not affiliated with NHL. -->\n')
         self.response.write('</div></body></html>')
