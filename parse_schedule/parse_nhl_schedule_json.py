f57893a0fa9a829163285fa743eea43fa0082453
Johan Guldmyr <martbhell@protonmail.com>
2017-10-04 13:52:08 +0300

    Use the API and not testdata.json
    and some documentation updates

--- parse_schedule/parse_nhl_schedule_json.py
+++ parse_schedule/parse_nhl_schedule_json.py
@@ -1,31 +1,31 @@
 #!/usr/bin/python
 
-# goal is to read json and write output in a format that the app can use as input
+# read json and write output in a format that the app can use as input
 
 import json # for parsing
 import urllib2 # for fetching data
 import sys # for return codes
 
-#url= 'https://statsapi.web.nhl.com/api/v1/schedule?startDate=2017-10-04&endDate=2018-04-09'
-#page = urllib2.urlopen(url)
+#TODO: put in dates in URL automagically
+#TODO: Run in a cronjob to update automagically daily?
+
+url= 'https://statsapi.web.nhl.com/api/v1/schedule?startDate=2017-10-04&endDate=2018-04-09'
+page = urllib2.urlopen(url)
 debug = True
 
-#jsondata = json.loads(page.read())
+jsondata = json.loads(page.read())
 
-fileread = open("testdata.json", "r")
-jsondata = json.loads(fileread.read())
-
+# want to use a file rather than fetch from the URL above?
+#fileread = open("testdata.json", "r")
+#jsondata = json.loads(fileread.read())
 #if debug: print jsondata
 
 data = {}
 data1 = []
 lines = []
-# Format2 test: { "Wed Jun 8, 2015" : [ "Tampa Bay", "Chicago" ] }
 teamdates = {}
 
-######## JSON
-
-# "schema":
+######## JSON "schema"
 
 # { "dates" : [
 #   { "date":"2017-10-05", 
@@ -52,28 +52,25 @@ teamdates = {}
 #     
 
 if jsondata['totalGames'] == 0:
-  print "totalGames found, adjust start and enddate"
+  print "totalGames not found in data, adjust start and enddate"
   sys.exit(31)
 
 dates = jsondata['dates']
 for key in dates:
   date = key['date']
   teamdates[date] = []
-  # convert date
   data1.append(date)
   games = key['games']
   for game in games:
     twoteams = []
     teams = game['teams']
-    # remove city name (keep only last word?)
-    # lowercase?
     twoteams.append(teams['away']['team']['name'])
     twoteams.append(teams['home']['team']['name'])
     teamdates[date].append(twoteams)
 
 # End result at some point needed to look like:
-#lines = set([u'Wednesday, Jun 15', u'Sunday, Jun 12'])
-#teamdates = {u'Wednesday, Jun 15': [[u'sharks', u'penguins']], u'Sunday, Jun 12': [[u'penguins', u'sharks']]}
+#        lines = set([u'2017-11-14', u'2018-04-04', u'2017-11-01'])
+#        teamdates = {u'2017-11-14': [[u'Buffalo Sabres', u'Pittsburgh Penguins'], [u'Columbus Blue Jackets', u'Montr\xe9al Canadiens'] ], u'2018-04-04': [[u'Ottawa Senators', u'Buffalo Sabres'], [u'Chicago Blackhawks', u'St. Louis Blues']] }
 
 # remove all duplicate entries - http://stackoverflow.com/questions/8200342/removing-duplicate-strings-from-a-list-in-python
 lines = set(data1)
@@ -81,5 +78,5 @@ lines = set(data1)
 linestr = "lines = " + str(lines).encode('utf8').replace("u'","'")
 print "lines = " + str(lines)
 #print linestr
-teamdatestr = "teamdates = " + str(teamdates).encode('utf8').replace("u'","'")
+#teamdatestr = "teamdates = " + str(teamdates).encode('utf8').replace("u'","'")
 print "teamdates = " + str(teamdates)
