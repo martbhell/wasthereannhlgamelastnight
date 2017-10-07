f7105d0ccdb69a076eb130bd1579963c93a2691e
Johan Guldmyr <johan.guldmyr@csc.fi>
2017-10-07 17:27:49 +0300

    content-type application/json and doc updates

--- src/update_schedule.py
+++ src/update_schedule.py
@@ -11,24 +11,24 @@ from google.appengine.api import app_identity
 import json # to parse URL
 import urllib2 # to fetch URL
 import datetime # to compose URL
-import tempfile
 
 debug = True
 
 # plan:
 # one, read/list file in the bucket in google cloud storage, grab checksum of file
 # two, run the parser (fetch upstream schedule, parse it). We could move the parser into this file or even better import it
+#  - moved it into this file, needed some modifications anyway
 # three, write the nhl schedule to a tempfile, calculate checksum
 # four, only if checksum is different than the existing one, write to bucket
 #  - skip four - is there a way to calculate etag? or then have to fetch file, calculate md5sum. Also have to write the JSON so that it's sorted
-# five, remove tempfile
-#  - meh
+# five, remove tempfiles
+#  - not using any tempfiles
 
 class MainPage(webapp2.RequestHandler):
   """Main page for GCS demo application."""
 
   def get(self):
-    """This get calls the other functions for some reason(tm)."""
+    """This get() calls the other functions for some reason(tm)."""
 
     bucket_name = os.environ.get('BUCKET_NAME',
                                  app_identity.get_default_gcs_bucket_name())
@@ -40,8 +40,9 @@ class MainPage(webapp2.RequestHandler):
 
     bucket = '/' + bucket_name
     filename = bucket + '/schedule'
-    self.tmp_filenames_to_clean_up = []
 
+    # This _etag is currently unused, could be used to reduce writes to only when the schedule is updated
+    #  (and to notify of updates)
     try:
         filename_etag = self.stat_file(filename).etag
         if debug: self.response.write("etag: %s \n" % filename_etag)
@@ -69,12 +70,11 @@ class MainPage(webapp2.RequestHandler):
     if totalGames == 0:
       pass
     else:
-      # parse
       self.response.write("Total Games: %s\n" % totalGames)
-      [ lines, teamdates ] = self.parse_schedule(jsondata)
-      content = self.make_data_json(lines,teamdates)
+      [ datelist, teamdates ] = self.parse_schedule(jsondata)
+      content = self.make_data_json(datelist,teamdates)
       self.create_file(filename, content)
-    if debug: self.read_file(filename)
+      if debug: self.read_file(filename)
 
   def create_file(self, filename, content):
       """Create a file."""
@@ -85,11 +85,10 @@ class MainPage(webapp2.RequestHandler):
       # retry params for this particular file handle.
       write_retry_params = gcs.RetryParams(backoff_factor=1.1)
       with gcs.open(
-          filename, 'w', content_type='text/plain', options={
+          filename, 'w', content_type='application/json', options={
               'x-goog-acl': 'private', 'x-goog-meta-type': 'schedule'},
               retry_params=write_retry_params) as cloudstorage_file:
                   cloudstorage_file.write(content)
-      self.tmp_filenames_to_clean_up.append(filename)
   
   def stat_file(self, filename):
       """ stat a file
@@ -100,7 +99,7 @@ class MainPage(webapp2.RequestHandler):
       return(stat)
 
   def fetch_upstream_schedule(self, url):
-      """ geturl a file
+      """ geturl a file and do some health checking
       """
 
       page = urllib2.urlopen(url)
@@ -137,18 +136,19 @@ class MainPage(webapp2.RequestHandler):
       return([set(list_of_dates), dict_of_keys_and_matchups])
 
   def make_data_json(self, lines, teamdates):
-      """ turn parsed data into json """
-
-#{
-# "dates": [ "2017-10-08", "2017-10-09" ], 
-# "teamdates": { "2017-12-30": [["Boston Bruins", "Ottawa Senators"]], "2017-12-21": [["Boston Bruins", "Ottawa Senators"]] }
-#}
+      """ turn parsed data into json, end result in JSON should look like:
+      {
+       "dates": [ "2017-10-08", "2017-10-09" ],
+       "teamdates": { "2017-12-30": [["Boston Bruins", "Ottawa Senators"]], "2017-12-21": [["Boston Bruins", "Ottawa Senators"]] }
+      }
+      """
       data = {}
       data['dates'] = list(lines) # can't be a set
       data['teamdates'] = teamdates
       json_data = json.dumps(data)
 
       return(json_data)
+
 
   def read_file(self, filename):
     self.response.write(
