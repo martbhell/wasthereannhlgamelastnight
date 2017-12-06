7bf0f8ad3ac3afb33ce2730016645a5f88864426
Johan Guldmyr <martbhell@protonmail.com>
2017-12-06 10:11:00 +0200

    Put CSS and menu in the same python

--- src/menu.py
+++ src/menu.py
@@ -1,5 +1,7 @@
 """ Menu, oor no? """
 
+# pylint: disable=too-few-public-methods
+
 import webapp2 # pylint: disable=import-error
 import wasthereannhlgamelastnight
 
@@ -22,7 +24,7 @@ class MainPage(webapp2.RequestHandler):
         <body style="text-align: center; padding-top: 5px;">\n\
         <div class="wrapper">')
         # Loop through and write all the teams like:
-        allteams = sorted(list(wasthereannhlgamelastnight.get_all_teams().keys()))
+        allteams = self.get_teams()
         for ateam in allteams:
             # https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Grid_Layout/Auto-placement_in_CSS_Grid_Layout
             # https://css-tricks.com/snippets/jquery/make-entire-div-clickable/
@@ -33,7 +35,47 @@ class MainPage(webapp2.RequestHandler):
 
         self.response.write('</body>\n\
         </html>\n')
+    @staticmethod
+    def get_teams():
+        """ Return a sorted list of teams """
+        allteams = sorted(list(wasthereannhlgamelastnight.get_all_teams().keys()))
+        return allteams
 
 APPLICATION = webapp2.WSGIApplication([
     ('/.*', MainPage),
+], debug=True)
+
+########
+
+class CSSPage(webapp2.RequestHandler):
+    """ CSS Page Class """
+
+    def get(self):
+        """Return CSS used to color boxes """
+
+        self.response.headers['Content-Type'] = 'text/css'
+        # Loop through and write all the teams:
+        allteams = sorted(list(wasthereannhlgamelastnight.get_all_teams().keys()))
+        # Recreate give_me_a_color classmethod because I couldn't figure out how to call it
+        colordict = {}
+        for ateam in allteams:
+            colors = wasthereannhlgamelastnight.get_team_colors(ateam)
+            backgroundcolor = colors[0]
+            try:
+                backgroundcolor2 = colors[1]
+            except IndexError:
+                backgroundcolor2 = colors[0]
+            if backgroundcolor == "000000":
+                backgroundcolor = backgroundcolor2
+            colordict[ateam] = backgroundcolor
+        # Make CSS
+        for ateam in allteams:
+            # https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Grid_Layout/Auto-placement_in_CSS_Grid_Layout
+            self.response.write('.wrapper > a.%s { \n\
+                background-color: #%s; \n\
+            }\n' % (ateam, colordict[ateam]))
+
+
+MENU_CSS = webapp2.WSGIApplication([
+    ('/.*', CSSPage),
 ], debug=True)
