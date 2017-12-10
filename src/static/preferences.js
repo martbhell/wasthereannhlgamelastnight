d785acdd2c7e8937450c31c6af4ac54dac5b0cd2
Johan Guldmyr <martbhell@protonmail.com>
2017-12-10 22:20:02 +0200

    only redirect to alphabetical sub pages
    Also move helpers up top for visibility.
    
    Use cooler !== undefined instead of typeof

--- src/static/preferences.js
+++ src/static/preferences.js
@@ -5,23 +5,32 @@
 function loadTeam() {
     // loadTeam we run on the main front page
     "use strict";
+    // Some helpers
+    var isAlpha = function (str) {
+	// because security
+        // https://stackoverflow.com/questions/2450641/validating-alphabetic-only-string-in-javascript
+        return /^[a-zA-Z]+$/.test(str);
+    };
+    // strnew is a function to return "/DET" from strnew("DET")
+    // https://stackoverflow.com/questions/41117799/string-interpolation-on-variable
+    var strnew = function (str) {
+        return `/${str}`;
+    };
+    ///////
     if (Storage === undefined) {
         console.log("browser does not support Web Storage");
     } else {
         if (!localStorage.team) {
             console.log("no team found in Web Storage, choose your team in the menu!");
         } else {
-        /* If we detect no arguments we use the team from localStorage  */
+        /* If we detect no arguments we use the team from localStorage, and only when alphabetical*/
+            var localstorageteam = localStorage.team;
+	    var isittrue = isAlpha(localstorageteam);
             if (argdate === "None") {
                 if (argteam === "None") {
-                    // console.log(localStorage.team);
-                    // strnew is a function to return "/DET" from strnew("DET")
-                    // https://stackoverflow.com/questions/41117799/string-interpolation-on-variable
-                    var strnew = function (str) {
-                        return `/${str}`;
-                    };
-                    // console.log(strnew(localStorage.team));
-                    location.replace(strnew(localStorage.team));
+                    if (isittrue) {
+                        location.replace(strnew(localstorageteam));
+                    }
                 }
             }
         }
@@ -31,7 +40,7 @@ function loadTeam() {
 function saveTeam(myteam) {
     // saveTeam we call via an onClick in the /menu
     "use strict";
-    if (typeof(Storage) !== "undefined") {
+    if (Storage !== undefined) {
         localStorage.team = myteam;
     }
 
