a0ca11a41b3ad1e6035a462f57d56dee7e1cbbbb
Johan Guldmyr <martbhell@protonmail.com>
2017-12-10 21:50:20 +0200

    More jslint improvements

--- src/static/preferences.js
+++ src/static/preferences.js
@@ -1,31 +1,38 @@
 /* https://www.w3schools.com/html/html5_webstorage.asp */
-"use strict";
+/*jslint browser */
+/*global argdate, argteam, console */
+
 function loadTeam() {
-// loadTeam we run on the main front page
-if (Storage === undefined) {
-  console.log("browser does not support Web Storage");
-} else {
-  if(!localStorage.team) {
-    console.log("no team found in Web Storage, choose your team in the menu!");
-  } else {
-/* If we detect no arguments we use the team from localStorage  */
-    if (argdate == "None") {
-      if (argteam == "None") {
-        // console.log(localStorage.team);
-        // strnew is a custom function to return /$team
-        // https://stackoverflow.com/questions/41117799/string-interpolation-on-variable
-        var strnew = function(str) { return `/${str}`; };
-        // console.log(strnew(localStorage.team));
-        window.location.replace(strnew(localStorage.team));
-	}
-      }
+    // loadTeam we run on the main front page
+    "use strict";
+    if (Storage === undefined) {
+        console.log("browser does not support Web Storage");
+    } else {
+        if (!localStorage.team) {
+            console.log("no team found in Web Storage, choose your team in the menu!");
+        } else {
+        /* If we detect no arguments we use the team from localStorage  */
+            if (argdate === "None") {
+                if (argteam === "None") {
+                    // console.log(localStorage.team);
+                    // strnew is a function to return "/DET" from strnew("DET")
+                    // https://stackoverflow.com/questions/41117799/string-interpolation-on-variable
+                    var strnew = function (str) {
+                        return `/${str}`;
+                    };
+                    // console.log(strnew(localStorage.team));
+                    location.replace(strnew(localStorage.team));
+                }
+            }
+        }
     }
 }
 
-}
-
 function saveTeam(myteam) {
-  // saveTeam we call via an onClick in the /menu
-  if (typeof(Storage) !== "undefined") { localStorage.team = myteam; }
+    // saveTeam we call via an onClick in the /menu
+    "use strict";
+    if (typeof(Storage) !== "undefined") {
+        localStorage.team = myteam;
+    }
 
 }
