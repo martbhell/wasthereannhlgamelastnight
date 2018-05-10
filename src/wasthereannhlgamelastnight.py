a008bc8b8990196ac2650d4efb625922bfbccbce
Johan Guldmyr <martbhell@protonmail.com>
2018-05-10 07:58:29 +0300

    anonymize_ip in ga

--- src/wasthereannhlgamelastnight.py
+++ src/wasthereannhlgamelastnight.py
@@ -558,13 +558,14 @@ def get_all_teams():
     }
     return allteams
 
+# https://developers.google.com/analytics/devguides/collection/gtagjs/ip-anonymization
 GOOGLE_ANALYTICS = """<!-- Global site tag (gtag.js) - Google Analytics -->
             <script async src='https://www.googletagmanager.com/gtag/js?id=UA-1265550-3'></script>
             <script>
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'UA-1265550-3');
+              gtag('config', 'UA-1265550-3', { 'anonymize_ip': true });
             </script>"""
 
 DISCLAIMER = '''<div class="disclaimer" style="font-size:10px; ">
