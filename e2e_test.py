7cbcd79875015fd17525c98b22f798a13c23d64e
Johan Guldmyr <martbhell@protonmail.com>
2019-01-02 23:56:30 +0200

    Turn e2e_test.py into black formatted

--- e2e_test.py
+++ e2e_test.py
@@ -9,13 +9,13 @@
 
 """ Do some testing """
 
-from __future__ import print_function # python3 
-import urllib2 # we validate that a website responds properly
-import sys # control exit codes
-import json # validate json
-import datetime # figure out year to have dynamic year testing
+from __future__ import print_function  # python3
+import urllib2  # we validate that a website responds properly
+import sys  # control exit codes
+import json  # validate json
+import datetime  # figure out year to have dynamic year testing
 
-HOST = 'https://testing-dot-wasthereannhlgamelastnight.appspot.com'
+HOST = "https://testing-dot-wasthereannhlgamelastnight.appspot.com"
 
 YESNO = ["YES\n", "NO\n"]
 
@@ -27,22 +27,23 @@ BOTH_YEARS = [str(THIS_YEAR), str(LAST_YEAR)]
 
 # This is a list of the basic tests / arguments.
 YESNODATES = [
-    str(THIS_YEAR) + '1013',
+    str(THIS_YEAR) + "1013",
     str(LAST_YEAR) + "1013",
     str(NEXT_YEAR) + "0316",
     "wingS/" + str(LAST_YEAR) + "1014",
     "wingS/" + str(NEXT_YEAR) + "0315",
     str(NEXT_YEAR) + "0315" + "/wingS",
-    '',
-    'WINGS',
-    'Lak',
-    'travis_e2e_test']
+    "",
+    "WINGS",
+    "Lak",
+    "travis_e2e_test",
+]
 
 # First we define two special URIs where we do some extra testing
 ARGS = {
-    'update_schedule':  {"test": ['accounts.google.com'], "type": "in"},
-    'get_schedule':     {"test": ['teamdates'], "type": "injson"},
-    'version':     {"test": BOTH_YEARS, "type": "in"},
+    "update_schedule": {"test": ["accounts.google.com"], "type": "in"},
+    "get_schedule": {"test": ["teamdates"], "type": "injson"},
+    "version": {"test": BOTH_YEARS, "type": "in"},
 }
 
 # Add the "basic" tests where we should only get a YES or NO
@@ -57,7 +58,7 @@ for arg in ARGS:
         sys.exit(66)
     html = response.read()
 
-    if ARGS[arg]['test'] == YESNO:
+    if ARGS[arg]["test"] == YESNO:
         print("asserting %s/%s - response code: %s" % (HOST, arg, response.code))
         try:
             assert html == "NO\n" or html == "YES\n"
@@ -66,17 +67,23 @@ for arg in ARGS:
             sys.exit(3)
 
     else:
-        print("asserting %s/%s contains %s - response code: %s" % (HOST, arg, ARGS[arg]['test'], response.code)) # pylint: disable=line-too-long
+        print(
+            "asserting %s/%s contains %s - response code: %s"
+            % (HOST, arg, ARGS[arg]["test"], response.code)
+        )  # pylint: disable=line-too-long
         try:
-            if ARGS[arg]['type'] == "in" or ARGS[arg]['type'] == "injson":
+            if ARGS[arg]["type"] == "in" or ARGS[arg]["type"] == "injson":
                 # this any loops over tests in ARGS[arg]['test']). There's also an all()
-                assert any(anarg in html for anarg in ARGS[arg]['test'])
+                assert any(anarg in html for anarg in ARGS[arg]["test"])
         except AssertionError:
             print("%s/%s does not contain %s" % (HOST, arg, ARGS[arg]))
             sys.exit(4)
-        if ARGS[arg]['type'] == "injson":
+        if ARGS[arg]["type"] == "injson":
             try:
-                assert json.loads(html)['teamdates'].popitem()
+                assert json.loads(html)["teamdates"].popitem()
             except KeyError:
-                print("popitem of JSON on %s/%s key %s failed" % (HOST, arg, ARGS[arg]['test']))
+                print(
+                    "popitem of JSON on %s/%s key %s failed"
+                    % (HOST, arg, ARGS[arg]["test"])
+                )
                 sys.exit(6)
