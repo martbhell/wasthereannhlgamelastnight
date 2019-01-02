46cabd98f33380fc21b9ec2b80f3719621ededaf
Johan Guldmyr <martbhell@protonmail.com>
2019-01-03 00:52:11 +0200

    black formatting

--- src/appengine_config.py
+++ src/appengine_config.py
@@ -3,4 +3,4 @@ from google.appengine.ext import vendor
 # https://github.com/GoogleCloudPlatform/python-docs-samples/tree/master/appengine/standard/storage/appengine-client
 # https://github.com/GoogleCloudPlatform/python-docs-samples/tree/master/appengine/standard
 # Add any libraries installed in the "lib" folder.
-vendor.add('lib')
+vendor.add("lib")
