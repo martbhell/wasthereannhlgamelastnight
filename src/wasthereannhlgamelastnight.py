2ccd3807168e32da539cf2021132033d5159af97
Johan Guldmyr <martbhell@protonmail.com>
2021-03-28 20:33:53 +0300

    Images are stored in /static by available via /

--- src/wasthereannhlgamelastnight.py
+++ src/wasthereannhlgamelastnight.py
@@ -256,12 +256,12 @@ DISCLAIMER = """<div class="disclaimer" style="font-size:10px; ">
               </div>"""
 COMMON_META = (
     """<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-            <link rel="shortcut icon" type="image/x-icon" href="static/favicon.ico"/>
-            <link rel="apple-touch-icon" sizes="180x180" href="static/apple-touch-icon.png">
-            <link rel="icon" type="image/png" sizes="32x32" href="static/favicon-32x32.png">
-            <link rel="icon" type="image/png" sizes="16x16" href="static/favicon-16x16.png">
-            <link rel="manifest" href="static/site.webmanifest">
-            <link rel="mask-icon" href="static/safari-pinned-tab.svg" color="#5bbad5">
+            <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico"/>
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+            <link rel="manifest" href="/site.webmanifest">
+            <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
             <meta name="msapplication-TileColor" content="#603cba">
             <meta name="robots" content="index,follow">
             <meta name="application-name" content="Was there an NHL game yesterday?">
