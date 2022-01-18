8305ba83a0b91a76e56da165abe949c8bb13ced5
Johan Guldmyr <martbhell@protonmail.com>
2022-01-18 21:02:21 +0200

    appengine_config.py is not used in py3

--- src/appengine_config.py
+++
@@ -1,6 +0,0 @@
-from google.appengine.ext import vendor
-
-# https://github.com/GoogleCloudPlatform/python-docs-samples/tree/master/appengine/standard/storage/appengine-client
-# https://github.com/GoogleCloudPlatform/python-docs-samples/tree/master/appengine/standard
-# Add any libraries installed in the "lib" folder.
-vendor.add("lib")
