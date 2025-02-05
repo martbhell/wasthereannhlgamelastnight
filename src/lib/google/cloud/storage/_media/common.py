a8b3b568ebf87d1949da978955d926e6606e5946
Continuous Integration <martbhell@users.noreply.github.com>
2025-02-05 10:22:23 +0000

    Rebuild src/lib after commit from dependabot

--- src/lib/google/cloud/storage/_media/common.py
+++ src/lib/google/cloud/storage/_media/common.py
@@ -1,4 +1,4 @@
-# Copyright 2020 Google LLC
+# Copyright 2017 Google Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,4 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "3.0.0"
+"""Common utilities for Google Media Downloads and Resumable Uploads.
+
+Includes custom exception types, useful constants and shared helpers.
+"""
+
+UPLOAD_CHUNK_SIZE = 262144  # 256 * 1024
+"""int: Chunks in a resumable upload must come in multiples of 256 KB."""
